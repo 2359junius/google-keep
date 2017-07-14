3d2a43ef10b5d25da7f6bd8124a81eadee4333e5
junius <junius.sim@2359media.com>
2017-07-14 15:48:52 +0700

    set up env variable for production

--- configs/env/production.js
+++ configs/env/production.js
@@ -1,11 +1,11 @@
 module.exports = {
     db: {
-        client: '',
+        client: 'mysql',
         connection: {
-            host: '',
-            user: '',
-            password: '',
-            database: 'googlekeep',
+            host: process.env.DATABASE_HOST,
+            user: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_NAME,
             charset: 'utf8'
         }
     }
--- knexfile-pre-deployment.js
+++ knexfile-pre-deployment.js
@@ -1,13 +1,17 @@
 // This file is different from src/knex.js. 
 // It is being used by the cli and its main purpose is to run migrations & seeds
-var config = require('./config')
-
 module.exports = {
     development: {
         migrations: {tableName: 'knex_migrations'},
         seeds: {tableName: './seeds'},
-        client: config.db.client,
-        connection: config.db.connection
+        client: 'mysql',
+        connection: {
+            host: '127.0.0.1', 
+            user: 'root',
+            password: 'Juniusmedia',
+            database: 'googlekeep',
+            charset: 'utf8'
+        }
     }
 }
 
