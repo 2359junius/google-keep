b35e919a3f01219260eb9f90a928a345616fbfef
junius <junius.sim@2359media.com>
2017-07-14 16:46:52 +0700

    s

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,8 @@
 // This file is different from src/knex.js. 
 // It is being used by the cli and its main purpose is to run migrations & seeds
 var config = require('./configs/config')
-
+console.log('config settings for prod', config)
+console.log('config client', config.db.client)
 module.exports = {
     development: {
         migrations: {tableName: 'knex_migrations'},
